eea05f0a9a227049c8287abd71d97fbc2b1326c2
nabiyddin <148422731+nabiyddin@users.noreply.github.com>
2023-11-16 09:05:22 +0300

    Bug fixes

---
+++ js/script.js
@@ -0,0 +1,85 @@
+const topCreatorsData = [
+  {
+    id: 1,
+    imgsrc: "../images/Avatar_keepitreal.png",
+    title: "Keepitreal",
+    totalSales: 34.53,
+  },
+
+  {
+    id: 2,
+    imgsrc: "../images/Avatar-DigiLab.png",
+    title: "DigiLab",
+    totalSales: 34.53,
+  },
+
+  {
+    id: 3,
+    imgsrc: "../images/Avatar-GravityOne.png",
+    title: "GravityOne",
+    totalSales: 34.53,
+  },
+
+  {
+    id: 4,
+    imgsrc: "../images/Avatar-Juanie.png",
+    title: "Juanie",
+    totalSales: 34.53,
+  },
+
+  {
+    id: 5,
+    imgsrc: "../images/Avatar-BlueWhale.png",
+    title: "BlueWhale",
+    totalSales: 34.53,
+  },
+
+  {
+    id: 6,
+    imgsrc: "../images/Avatar-Mr Fox.png",
+    title: "Mr Fox",
+    totalSales: 34.53,
+  },
+
+  {
+    id: 7,
+    imgsrc: "../images/Avatar-Shroomie.png",
+    title: "Shroomie",
+    totalSales: 34.53,
+  },
+
+  {
+    id: 8,
+    imgsrc: "../images/Avatar-Robotica.png",
+    title: "Robotica",
+    totalSales: 34.53,
+  },
+
+  {
+    id: 9,
+    imgsrc: "../images/Avatar-RustyRobot.png",
+    title: "RustyRobot",
+    totalSales: 34.53,
+  },
+
+  {
+    id: 10,
+    imgsrc: "../images/Avatar-Animakid.png",
+    title: "Animakid",
+    totalSales: 34.53,
+  },
+
+  {
+    id: 11,
+    imgsrc: "../images/Avatar-Dotgu.png",
+    title: "Dotgu",
+    totalSales: 34.53,
+  },
+
+  {
+    id: 12,
+    imgsrc: "../images/Avatar-Ghiblier.png",
+    title: "Ghiblier",
+    totalSales: 34.53,
+  },
+];
